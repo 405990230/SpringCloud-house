df9d6eedd56d0fb23608b490367c8a3a8cc51afe
严都 <du.yan@yintech.cn>
2020-08-19 15:17:14 +0800

    no message

--- house-comment/src/main/java/com/mooc/boss/house/comment/config/DuridConfig.java
+++ house-comment/src/main/java/com/mooc/boss/house/comment/config/DuridConfig.java
@@ -3,7 +3,6 @@ package com.mooc.boss.house.comment.config;
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +36,7 @@ public class DuridConfig {
 
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
-        return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        return null;
     }
 }
---
+++ house-user/src/main/java/com/mooc/boss/house/user/utils/Test.java
@@ -0,0 +1,8 @@
+package com.mooc.boss.house.user.utils;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "湖北省";
+        System.out.println(str.indexOf("省"));
+    }
+}
